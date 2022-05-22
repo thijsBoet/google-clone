15942673e225a991678dd73a58d3aa38febce2b0
thijsBoet <m.boet@chello.nl>
2022-05-22 19:36:12 +0200

    added header, body and footer components

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Header from '../components/Header';
+import Body from '../components/Body';
+import Footer from '../components/Footer';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Google Clone</title>
-        <meta name="description" content="Clone of search engine Google" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div>
+			<Head>
+				<title>Google Clone</title>
+				<meta name='description' content='Clone of search engine Google' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
 
-      <h1>Hello World</h1>
-    </div>
-  )
+			<Header />
+
+			<Body />
+
+			<Footer />
+		</div>
+	);
 }
